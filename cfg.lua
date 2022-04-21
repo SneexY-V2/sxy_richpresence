5acebb17224c353c71235ad7259adc5c2c324770
SneexY <74669652+SneexY-V2@users.noreply.github.com>
2022-04-21 17:06:40 -0400

    Added By SneexY#7010

---
+++ cfg.lua
@@ -0,0 +1,19 @@
+cfg = {}
+--This is where you put your DiscordAppID you can get it from here https://discord.com/developers/applications
+cfg.DiscordAppID =  
+
+cfg.discordImageName = 'sans_titre_1_'
+cfg.hoverText = '🌴Utopia RP | FR🌴'
+cfg.smallDiscordImageName = '.....'
+cfg.smallHoverText = 'Rejoins-nous voyons !'
+
+cfg.button1 = {
+    text = 'Rejoins-nous sur Discord !',
+    url = 'https://discord.gg/UY3vawNdzS'
+}
+
+cfg.button2 = {
+    text = 'Rejoins-nous sur Discord !',
+    url = 'https://discord.gg/UY3vawNdzS'
+}
+-- Just to clarify something, you can only have 2 buttons
